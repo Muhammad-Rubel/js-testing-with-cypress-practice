203d68e40295d918d29b871423b8d6a90ed46d53
Muhammad-Rubel <rubel.contact@gmail.com>
2022-12-19 15:49:47 +0600

    Made some change

--- cypress/e2e/spec.cy.js
+++
@@ -1,9 +0,0 @@
-/// <reference types="Cypress" />
-
-describe('My First Test', () => {
-  it('finds the content "type" and click it', () => {
-    cy.visit('https://example.cypress.io');
-
-    cy.contains('type').click();
-  });
-});
